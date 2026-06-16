cb25262b926049e18b81517a223063bff7fafa0e
geoezzt <59900471+geoezzt@users.noreply.github.com>
2026-06-16 09:01:06 +0400

    Refactor URL checking to use async functions

--- scanner.py
+++ scanner.py
@@ -1,4 +1,6 @@
 import requests
+import asyncio
+import aiohttp
 
 def get_wayback_urls(domain):
     url = f"http://web.archive.org/cdx/search/cdx?url={domain}/*&output=json&fl=original&collapse=urlkey"
@@ -9,10 +11,10 @@ def get_wayback_urls(domain):
     except:
         return []
 
-def check_url(url):
+async def check_url(session, url):
     try:
-        r = requests.get(url, timeout=5, allow_redirects=False)
-        status = r.status_code
+        async with session.get(url, timeout=3, allow_redirects=False) as r:
+            status = r.status
     except:
         status = 0
 
@@ -28,3 +30,36 @@ def check_url(url):
         url_type = 'json'
 
     return {'url': url, 'status': status, 'type': url_type, 'params': params}
+
+async def check_urls_async(urls):
+    results = {
+        '200': [], '403': [], '5xx': [], '302': [],
+        'params': [], 'js': [], 'json': [], 'other': []
+    }
+
+    # 100 request في نفس الوقت = سرعة صاروخ
+    connector = aiohttp.TCPConnector(limit=100, ssl=False)
+    timeout = aiohttp.ClientTimeout(total=5)
+
+    async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
+        tasks = [check_url(session, url) for url in urls]
+        responses = await asyncio.gather(*tasks)
+
+    for res in responses:
+        status = res['status']
+        url = res['url']
+
+        if status == 200: results['200'].append(url)
+        elif status == 403: results['403'].append(url)
+        elif status in [500, 502, 503]: results['5xx'].append(url)
+        elif status in [301, 302, 307]: results['302'].append(url)
+        else: results['other'].append(url)
+
+        if res['type'] == 'param':
+            results['params'].append(f"{url} | {res['params']}")
+        elif res['type'] == 'js':
+            results['js'].append(url)
+        elif res['type'] == 'json':
+            results['json'].append(url)
+
+    return results
